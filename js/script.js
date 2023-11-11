114901ade42ec52db056eed55eead93583e7890b
Josh Warner <joshua.ryan.warner@icloud.com>
2023-11-11 15:28:54 +1100

    Update

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 $(document).ready(function () { 
 
-    $('.sort').click(function() {
-         $(".sortMenu").animate({
-            left: '-=280'
-        }, 300 );
+    $(".menu").hide();
+    $(".menu-list li").hide();
+    
+    $( "header .open" ).click(function() {
+        $( ".menu" ).fadeIn(400);
+        $(".menu-list li").delay(200).each(function(i) {
+            $(this).delay(100 * i).fadeIn(700);
+        });
+        $('html, body').css({
+            overflow: 'hidden',
+            height: '100%'
+        });
     });
-    $('.close').click(function() {
-         $(".sortMenu").animate({
-            left: '+=280'
-        }, 300 );
+    
+    $( "header .close" ).click(function() {
+        $( ".menu" ).fadeOut(400);
+        $(".menu-list li").fadeOut(400);
+        $('html, body').css({
+            overflow: 'auto',
+            height: 'auto'
+        });
     });
+    
     
 });
